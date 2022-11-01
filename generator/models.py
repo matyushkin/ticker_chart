4bba569a5c5663609d3887289bc4280dbaf89807
matyushkin <leva.matyushkin@gmail.com>
2022-11-01 21:20:25 +0300

    Correction of ticker fields names

--- generator/models.py
+++ generator/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Ticker(models.Model):
-    ticker_name = models.CharField(max_length=100)
-    ticker_time = models.DateTimeField()
-    ticker_price = models.IntegerField()
+    name = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    price = models.IntegerField()
 
     def __str__(self):
-        return f'{self.ticker_name, self.ticker_time.strftime("%Y-%m-%d %H:%M:%S")}'
+        return f'{self.name, self.time.strftime("%Y-%m-%d %H:%M:%S")}'
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['ticker_name', 'ticker_time'], name='unique_ticker_time_combination'
+                fields=['name', 'time'], name='unique_name_time_combination'
             )
         ]
-        get_latest_by = ['ticker_name', 'ticker_time']
+        get_latest_by = ['name', 'time']
     
 
--- scripts/generator.py
+++ scripts/generator.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from generator.models import Ticker
 
 TICKERS_N = 100
+TIME_STEP = 1   # in seconds
 
 def generate_movement():
     movement = -1 if random() < 0.5 else 1
@@ -16,14 +17,19 @@ def generate_movement():
 #         pass
 
 def run():
-    if Ticker.objects.all():
-        pass
-    else:
-        for i in range(TICKERS_N):
+    current_time = datetime.datetime.now(tz=timezone.utc)
+    for i in range(TICKERS_N):
+        name_i=f"ticker_{i:02d}
+        last_item = Ticker.objects.filter(name=name_i).last()
+        if last_item:
+            pass
+        else:
             ticker = Ticker(
-                ticker_name = f"ticker_{i:02d}",
-                ticker_time = datetime.datetime.now(tz=timezone.utc),
-                ticker_price = 0
+                name = f"ticker_{i:02d}",
+                time = current_time,
+                price = 0
             )
             ticker.save()
+
+
     #populate_db()
