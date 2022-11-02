1b8e9d9894e17962ba97a27fd30b468ba5f3d326
matyushkin <leva.matyushkin@gmail.com>
2022-11-02 11:22:50 +0300

    Precise sleeping for tickers is corrected

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,5 +1,5 @@
-from time import sleep
-from datetime import datetime, timedelta
+import sched, time
+from datetime import date, datetime, timedelta
 from random import random
 from django.utils import timezone
 
@@ -8,34 +8,44 @@ from generator.models import Ticker
 TICKERS_N = 100
 TIME_STEP = 1   # in seconds
 
+
+def perf_sleep(duration, get_now=time.perf_counter):
+    now = get_now()
+    end = now + duration
+    while now < end:
+        now = get_now()
+
+
 def generate_movement():
     movement = -1 if random() < 0.5 else 1
     return movement
 
 
-def populate_db(current_time):
-    if Ticker.objects.all():
-        last_time = Ticker.objects.latest().time
-        dt = (current_time - last_time)
-        if dt >= timedelta(seconds=TIME_STEP):
-            for i in range(TICKERS_N):
-                name_i = f"ticker_{i:02d}"
-                last_item = Ticker.objects.filter(name=name_i).last()
-                ticker = Ticker(name = name_i, time = current_time,
-                                price = last_item.price + generate_movement())
-                ticker.save()
-        else:
-            sleep(dt.microseconds/1000000.0)
-    else:
-        for i in range(TICKERS_N):
-            name_i = f"ticker_{i:02d}"
-            ticker = Ticker(name = name_i, time = current_time, price = 0)
-            ticker.save()
+def populate_db():
+    current_time = datetime.now(tz=timezone.utc)
+    for i in range(TICKERS_N):
+        name_i = f"ticker_{i:02d}"
+        last_item = Ticker.objects.filter(name=name_i).last()
+        ticker = Ticker(name = name_i,
+                        time = current_time,
+                        price = last_item.price + generate_movement())
+        ticker.save()
 
 
 def run():
-    while True:
-        try:
-            populate_db(current_time = datetime.now(tz=timezone.utc))
-        except KeyboardInterrupt:
-            break
+    clocks = time.time()
+    s = sched.scheduler(time.time, perf_sleep)
+    if not Ticker.objects.all():
+        for i in range(TICKERS_N):
+            ticker = Ticker(name = f"ticker_{i:02d}",
+                            time = datetime.now(tz=timezone.utc),
+                            price = 0)
+            ticker.save()
+    else:
+        while True:
+            try:
+                clocks += TIME_STEP
+                s.enterabs(clocks, 0, populate_db)
+                s.run()
+            except KeyboardInterrupt:
+                break
